8289ac0f60b174c9e3a318c6f4eec44724473db3
Margarida Martelo <margmartelo@gmail.com>
2021-05-05 17:10:57 +0100

    Refactoring spec file

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,14 +1,16 @@
 require 'address_book.rb'
 
 describe AddressBook do
+    let(:address_book) { AddressBook.new({ Smith: "07512345678", Williams: "07587654321" }) }
+    
     it "should return the phone number associated with a provided surname" do
-        address_book = AddressBook.new({ Smith: "07512345678", Williams: "07587654321" })
+      #  address_book = AddressBook.new({ Smith: "07512345678", Williams: "07587654321" })
         expect(address_book.find_by("Smith")).to eq "07512345678"
         expect(address_book.find_by("Williams")).to eq "07587654321"
     end
 
     it "should raise an error if provided surname is not associated with a phone number" do
-        address_book = AddressBook.new({ Smith: "07512345678", Williams: "07587654321" })
+       # address_book = AddressBook.new({ Smith: "07512345678", Williams: "07587654321" })
         expect {address_book.find_by("Jones")}.to raise_error("Couldn't find any entry that matches Jones")
     end
 end
