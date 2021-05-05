dff1976fe5dced85a8464ad7d7d230ce4aa5967b
Margarida Martelo <margmartelo@gmail.com>
2021-05-05 16:47:15 +0100

    Adding test for the case a provided surname is not associated with a phone number

--- lib/address_book.rb
+++ lib/address_book.rb
@@ -6,6 +6,10 @@ class AddressBook
 
     def find_by(surname)
         surname = surname.to_sym
-        return @list_of_entries[surname] 
+        if @list_of_entries.key?(surname)
+            return @list_of_entries[surname] 
+        else
+           raise StandardError.new("Couldn't find any entry that matches #{surname}")
+        end
     end
 end
