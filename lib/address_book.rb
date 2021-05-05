693ada3dc9c8d9dcf474d5dcc3f2846214a4d92b
Margarida Martelo <margmartelo@gmail.com>
2021-05-05 17:24:49 +0100

    Adding test for the case a provided surname is an empty string

--- lib/address_book.rb
+++ lib/address_book.rb
@@ -8,7 +8,9 @@ class AddressBook
         surname = surname.to_sym
         if @list_of_entries.key?(surname)
             return @list_of_entries[surname] 
-        else
+        elsif surname.to_s == " "
+            raise StandardError.new("Please provide the surname of the contact you wish to get")
+        else    
            raise StandardError.new("Couldn't find any entry that matches #{surname}")
         end
     end
